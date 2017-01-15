29cb8f9dc0099507f5bde200a8931be7ed5177fd
Rohit Kumar Yadav <rohit.yadav848@yahoo.com>
2017-01-15 21:51:09 +0530

    codechef problem

--- chef_game1.py
+++ chef_game1.py
@@ -1,17 +1,17 @@
 def nb_move(s):
-    c,h,e,f=0,0,0,0
-    for i in range(0,len(s)):
-         if s[i]=='C':
-            c+=1
-         elif s[i]=='H' and c>0:
-             h=h+1
-             c=c-1
-         elif s[i]=='E' and h>0:
-             e=e+1
-             h=h-1
-         elif s[i]=='F' and e>0:
-             f=f+1
-             e=e-1
-    return f
- 
+	c, h, e, f = 0,0,0,0
+
+	for i in range(0,len(s)):
+		if s[i]=='C':
+			c+=1
+		elif s[i]=='H' and c>0:
+			h=h+1
+			c=c-1
+		elif s[i]=='E' and h>0:
+			e=e+1
+			h=h-1
+		elif s[i]=='F' and e>0:
+			f=f+1
+			e=e-1
+	return f
 print(nb_move(input()))   
