f1f9107521a53ec5a8ad2af8364f274239eaaa18
rohit kumar yadav <rohit.yadav848@yahoo.com>
2017-01-28 15:50:27 +0530

    irenovate

--- Forgotten_language1.py
+++ Forgotten_language1.py
@@ -1,4 +1,5 @@
 T = int(input("enter the number of tests: "))
+
 while T>0:
 	N, K = map(int, input("Enter the values of N and K: ").split())
 	print("Enter the list of strings here: ")
@@ -11,6 +12,7 @@ while T>0:
 			y = input("Enter the string: ")
 			S.append(y)
 	S = set(S)	
+	
 	for x in range(len(D)):
 		if D[x] in S:
 			print("YES", end=' ')
