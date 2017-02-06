67689dc547a4553f64098cf263df08df53e21052
rohit kumar yadav <rohit.yadav848@yahoo.com>
2017-02-06 11:40:46 +0530

    codechef

--- measurement.py
+++ measurement.py
@@ -1,5 +1,6 @@
 total_measurements = int(input("Enter the total no. of inputs: "));
 measurements = [];
+
 for x in range(total_measurements):
 	x = int(input("Enter the measurement: "));
 	measurements.append(x);
