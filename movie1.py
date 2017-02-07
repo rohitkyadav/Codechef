7f9f9b11e0b19b85679ae99d36be8c43f36aa893
rohit kumar yadav <rohit.yadav848@yahoo.com>
2017-02-07 20:39:44 +0530

    codechef

--- movie1.py
+++ movie1.py
@@ -2,6 +2,7 @@ for _ in range(int(input())):
 	n = int(input())
 	l, r = list(map(int,input().split())), list(map(int,input().split()))
 	mp, mr, idx = 0, 0, 0
+	
 	for i in range(n):
 		if l[i]*r[i] > mp:
 			mp = l[i]*r[i]
