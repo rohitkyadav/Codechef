2137c6d21ea8cbb30db231729463f1441c068acd
rohit kumar yadav <rohit.yadav848@yahoo.com>
2017-04-05 00:47:12 +0530

    hackerrank problem

---
+++ time_conversion.py
@@ -0,0 +1,16 @@
+"""
+https://www.hackerrank.com/challenges/time-conversionon
+"""
+
+#!/bin/python3
+
+import sys
+
+s = input()
+zn = s[-2:]
+if zn == "PM" and s[:2] != "12":
+    s = str(12 + int(s[:2])) + s[2:]
+if zn == "AM" and s[:2] == "12":
+    s = "00" + s[2:]
+
+print (s[:-2])
